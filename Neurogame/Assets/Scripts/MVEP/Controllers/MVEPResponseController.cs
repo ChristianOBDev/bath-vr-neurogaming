f3b7e9adcf48df50d7beb3b35b5fd64fce6d1a81
ChristianAtBath <christianob.dev@gmail.com>
2026-02-24 11:34:55 +0000

    Rename ResponseController to MVEPResponseController
    Rename the controller source file and class from ResponseController to MVEPResponseController to align the class name with the new filename and MVEP module naming. The .meta file was updated accordingly to reflect the rename.

--- Neurogame/Assets/Scripts/MVEP/Controllers/MVEPResponseController.cs
+++ Neurogame/Assets/Scripts/MVEP/Controllers/MVEPResponseController.cs
@@ -1,7 +1,7 @@
 using System.Linq;
 using UnityEngine;
 
-public class ResponseController : MonoBehaviour
+public class MVEPResponseController : MonoBehaviour
 {
   [SerializeField] private CanoeController canoeController;
   private Phase phase;
