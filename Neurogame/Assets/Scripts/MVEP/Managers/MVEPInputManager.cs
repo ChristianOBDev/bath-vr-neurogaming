0c8890c884fec96ebe8c1a2029e64ebbbfec9dee
ChristianAtBath <christianob.dev@gmail.com>
2026-03-11 10:31:49 +0000

    Update MVEPInputManager.cs

--- Neurogame/Assets/Scripts/MVEP/Managers/MVEPInputManager.cs
+++ Neurogame/Assets/Scripts/MVEP/Managers/MVEPInputManager.cs
@@ -4,6 +4,7 @@ using System;
 
 public class MVEPInputManager : MonoBehaviour
 {
+  // Lane input references
   [SerializeField] private InputActionReference lane1;
   [SerializeField] private InputActionReference lane2;
   [SerializeField] private InputActionReference lane3;
@@ -12,12 +13,64 @@ public class MVEPInputManager : MonoBehaviour
 
   public static Action<int> OnLaneInput;
 
+  // Test inputs
+  [SerializeField] private InputActionReference spawnChunks;
+  [SerializeField] private InputActionReference startGame;
+  [SerializeField] private InputActionReference pauseGame;
+  [SerializeField] private InputActionReference quitGame;
+
+  [SerializeField] private InputActionAsset inputActions;
+
   void OnEnable()
   {
+    inputActions.Enable();
+
     lane1.action.performed += (ctx) => OnLaneInput?.Invoke(0);
     lane2.action.performed += (ctx) => OnLaneInput?.Invoke(1);
     lane3.action.performed += (ctx) => OnLaneInput?.Invoke(2);
     lane4.action.performed += (ctx) => OnLaneInput?.Invoke(3);
     lane5.action.performed += (ctx) => OnLaneInput?.Invoke(4);
+
+    spawnChunks.action.performed += SpawnChunks;
+    startGame.action.performed += StartGame;
+    pauseGame.action.performed += PauseGame;
+    quitGame.action.performed += QuitGame;
   }
+
+  void OnDisable()
+  {
+    inputActions.Disable();
+
+    lane1.action.performed -= (ctx) => OnLaneInput?.Invoke(0);
+    lane2.action.performed -= (ctx) => OnLaneInput?.Invoke(1);
+    lane3.action.performed -= (ctx) => OnLaneInput?.Invoke(2);
+    lane4.action.performed -= (ctx) => OnLaneInput?.Invoke(3);
+    lane5.action.performed -= (ctx) => OnLaneInput?.Invoke(4);
+
+    spawnChunks.action.performed -= SpawnChunks;
+    startGame.action.performed -= StartGame;
+    pauseGame.action.performed -= PauseGame;
+    quitGame.action.performed -= QuitGame;
+  }
+
+  void SpawnChunks(InputAction.CallbackContext context)
+  {
+    MVEPGameManager.Instance.SpawnChunks();
+  }
+
+  void StartGame(InputAction.CallbackContext context)
+  {
+    MVEPGameManager.Instance.StartGame();
+  }
+
+  void PauseGame(InputAction.CallbackContext context)
+  {
+    MVEPGameManager.Instance.PauseGame();
+  }
+
+  void QuitGame(InputAction.CallbackContext context)
+  {
+    MVEPGameManager.Instance.QuitGame();
+  }
+
 }
