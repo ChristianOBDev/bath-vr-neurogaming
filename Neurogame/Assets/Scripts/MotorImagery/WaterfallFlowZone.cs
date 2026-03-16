be717cda31c23e2c53ce5c92fe5f8033fad1a62e
hectorlowe <hector.lowe@gmail.com>
2026-03-16 11:34:45 +0000

    Feature motorimagery orientation (#37)
    * Bug Fixes
    
    SO many bugs.
    -Spawner bugs
    -Ball getting lodged in kicker
    -Orientation of flowzone
    -Orientation of ball flight
    -Ball overshooting
    -Ball Undershooting
    -Idle state all screwy
    -Center line guidance
    -Added a 'pulse' tween
    -Added a glow visual feedback to show 'charge' of left/right (needs refinement.)
    
    * Swell Scale Change
    
    Simply altered the swell tween of the kickers when they fire.
    
    * Ongoing Glow Fix
    
    * Kicker Glow Fixed
    
    Kickers now ramp glow in phase one. They should also glow in response to player input.
    
    Added debug-logging toggle to the kickers for testing.

--- Neurogame/Assets/Scripts/MotorImagery/WaterfallFlowZone.cs
+++ Neurogame/Assets/Scripts/MotorImagery/WaterfallFlowZone.cs
@@ -4,39 +4,36 @@ using UnityEngine;
 public class WaterfallFlowZone : MonoBehaviour
 {
     [Header("Flow Settings")]
-
-    [Tooltip("Direction the water flows in world space")]
+    [Tooltip("Direction the water flows in LOCAL space")]
     public Vector3 flowDirection = Vector3.back;
-
     [Tooltip("Speed of water flow")]
     public float flowSpeed = 4f;
-
     [Tooltip("Blend gravity into flow")]
     public bool applyGravity = false;
+
+    private Vector3 WorldFlowDirection =>
+        SceneOrientation.Instance != null
+            ? SceneOrientation.Instance.Resolve(flowDirection.normalized)
+            : flowDirection.normalized;
 
     private void OnTriggerEnter(Collider other)
     {
         BallController ball = other.GetComponent<BallController>();
         if (ball != null)
-        {
-            ball.EnterWaterfall(flowDirection.normalized, flowSpeed, applyGravity);
-        }
+            ball.EnterWaterfall(WorldFlowDirection, flowSpeed, applyGravity);
     }
-    private void OnTriggerExit(Collider other)
-    {
-        BallController ball = other.GetComponent<BallController>();
-        if (ball != null)
-        {
-            ball.ExitWaterfall();
-        }
-    }
+
     private void OnTriggerStay(Collider other)
     {
         BallController ball = other.GetComponent<BallController>();
         if (ball != null && ball.CurrentState != BallState.OnWaterfall)
-        {
-            ball.EnterWaterfall(flowDirection.normalized, flowSpeed, applyGravity);
-        }
+            ball.EnterWaterfall(WorldFlowDirection, flowSpeed, applyGravity);
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        BallController ball = other.GetComponent<BallController>();
+        if (ball != null)
+            ball.ExitWaterfall();
+    }
 }
