a8e13f133f74d9a2841eab493dbc442b31cb835d
ChristianAtBath <christianob.dev@gmail.com>
2026-03-23 12:37:33 +0000

    Unload scenes on Exit

--- Neurogame/Assets/Scripts/SceneManagement/Objects/SceneStreamingVolume.cs
+++ Neurogame/Assets/Scripts/SceneManagement/Objects/SceneStreamingVolume.cs
@@ -12,6 +12,7 @@ public class SceneStreamingVolume : MonoBehaviour
 
   private void OnTriggerEnter(Collider other)
   {
+    Debug.Log("Player entered streaming volume: " + gameObject.name);
     if (!other.CompareTag("Player"))
       return;
 
@@ -19,5 +20,15 @@ public class SceneStreamingVolume : MonoBehaviour
       SceneStreamingManager.Instance.RequestLoad(config);
     if (config.scenesToUnload.Count > 0)
       SceneStreamingManager.Instance.RequestUnload(config);
+  }
+
+  private void OnTriggerExit(Collider other)
+  {
+    if (!other.CompareTag("Player"))
+      return;
+
+    Debug.Log("Player exited streaming volume: " + gameObject.name);
+    if (config.scenesToLoad.Count > 0)
+      SceneStreamingManager.Instance.RequestUnloadOnExit(config);
   }
 }
